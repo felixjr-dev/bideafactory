eec5f3ddf2d6d788b396a9bc1799f81e8f8c9282
Felix Diaz <fdiazgal@everis.com>
2020-01-12 15:49:08 -0500

    first commit

---
+++ test/KAF40210-1-0/src/test/java/cl/wom/middleware/consumer/ConsumerInventoryAdjusmentApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.wom.middleware.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerInventoryAdjusmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
